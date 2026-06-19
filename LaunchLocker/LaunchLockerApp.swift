df996d81ad66d6ae723b536599f9cc2639e1a76f
Allison Jiang <allison.x.jiang@gmail.com>
2026-06-18 20:19:29 -0700

    Initial Commit

---
+++ LaunchLocker/LaunchLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchLockerApp.swift
+//  LaunchLocker
+//
+//  Created by AJ on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct LaunchLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
